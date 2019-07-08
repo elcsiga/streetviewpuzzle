459d7448c03fa4404020b9a0731e08dec25b2427
Andras Kovacs <elcsiga@gmail.com>
2019-07-08 11:33:47 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import * as privateAppConfig from '../../app-config-private.json';
+import { AuthService } from './auth/auth-service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,37 +9,11 @@ import * as privateAppConfig from '../../app-config-private.json';
 export class AppComponent {
   title = 'streetviewpuzzle';
 
-  constructor() {
-    firebase.initializeApp(privateAppConfig.firebase);
-
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log('AUTH', user);
-        // User is signed in.
-        // var displayName = user.displayName;
-        // var email = user.email;
-        // var emailVerified = user.emailVerified;
-        // var photoURL = user.photoURL;
-        // var isAnonymous = user.isAnonymous;
-        // var uid = user.uid;
-        // var providerData = user.providerData;
-        // ...
-      } else {
-        console.log('AUTHOUT');
-        // ...
-      }
-    });
+  constructor(
+    private authService: AuthService
+  ) {
+    authService.init();
   }
 
-  register(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then( re => console.log('REG', re))
-    .catch( error => console.error('REG', error));
-  } 
-  
-  login(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then( re => console.log('LOGIN', re))
-    .catch( error => console.error('LOGIN', error));
-  }
+
 }
--- src/app/auth/auth-service/auth.service.ts
+++ src/app/auth/auth-service/auth.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
 
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import * as privateAppConfig from '../../../../app-config-private.json';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   constructor() { }
+
+  init() {
+    firebase.initializeApp(privateAppConfig.firebase);
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log('AUTH', user);
+        // User is signed in.
+        // var displayName = user.displayName;
+        // var email = user.email;
+        // var emailVerified = user.emailVerified;
+        // var photoURL = user.photoURL;
+        // var isAnonymous = user.isAnonymous;
+        // var uid = user.uid;
+        // var providerData = user.providerData;
+        // ...
+      } else {
+        console.log('AUTHOUT');
+        // ...
+      }
+    });
+  }
+
+
+  register(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then( re => console.log('REG', re))
+    .catch( error => console.error('REG', error));
+  } 
+  
+  login(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then( re => console.log('LOGIN', re))
+    .catch( error => console.error('LOGIN', error));
+  }
 }
