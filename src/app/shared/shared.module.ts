fa8b435a81359073eca7c6cb6da14b261d9347bc
Andras Kovacs <elcsiga@gmail.com>
2019-07-11 00:34:33 +0200

    map

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogComponent } from './dialog/dialog.component';
+import { MaterialModule } from '../material/material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [DialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule
   ],
   exports: [DialogComponent]
 })
